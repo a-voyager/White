f9870a26b9ad5fc1d911b39e3ad1497df8b10db8
wuhaojie <w19961009@126.com>
2016-06-22 19:47:42 +0800

    add base.

---
+++ app/src/main/java/top/wuhaojie/white/base/BaseActivity.java
@@ -0,0 +1,15 @@
+package top.wuhaojie.white.base;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by wuhaojie on 2016/6/22 19:45.
+ */
+public class BaseActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
---
+++ app/src/main/java/top/wuhaojie/white/base/BaseApplication.java
@@ -0,0 +1,9 @@
+package top.wuhaojie.white.base;
+
+import android.app.Application;
+
+/**
+ * Created by wuhaojie on 2016/6/22 19:44.
+ */
+public class BaseApplication extends Application {
+}
